e3322b9142e0943e6b58dbaf96673578e9bd7879
Kryuell <Kryuel17@gmail.com>
2023-09-12 22:05:35 +0100

    1 Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
-
+/**
+ * print_alphabet - Prints the lowercase English alphabet followed by a newline.
+ *
+ * Description: This function prints the alphabet from 'a' to 'z' in a
+ * sequential order and appends a newline character '\n' at the end.
+ */
 void print_alphabet(void)
 {
-	char a = 'a';
+    char a = 'a';
 
-	while (a <= 'z')
-	{
-		_putchar(a);
-		a++;
-	}
-	_putchar('\n');
-}
+    while (a <= 'z')
+    {
+        _putchar(a);
+        a++;
+    }
+    _putchar('\n');
+}}
