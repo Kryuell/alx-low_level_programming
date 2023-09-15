c6fa4f3cb70320398134cb401dc2d47ee584b482
Kryuell <Kryuel17@gmail.com>
2023-09-15 21:11:02 +0100

    Advanced

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ * @size: param for int
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int i, z;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (z = 1; z <= size; z++)
+		{
+			if ((i + z) <= size)
+			{
+				_putchar(' ');
+			}
+			else
+				_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
