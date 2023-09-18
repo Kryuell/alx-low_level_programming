5be5f3b2c6852ebb0618601ce9625b0b8960fa23
Kryuell <Kryuel17@gmail.com>
2023-09-18 18:00:45 +0100

    Swap

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int a;
+    int b;
+
+    a = 98;
+    b = 42;
+    
+    printf("a=%d, b=%d\n", a, b);
+    
+    swap_int(&a, &b); /* Call the function to swap a and b */
+
+    printf("a=%d, b=%d\n", a, b);
+
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap_int(int *a, int *b)
+{
+    int temp;
+
+    /* Store the value of a in temp */
+    temp = *a;
+
+    /* Assign the value of b to a */
+    *a = *b;
+
+    /* Assign the value of temp (original a) to b */
+    *b = temp;
+}
