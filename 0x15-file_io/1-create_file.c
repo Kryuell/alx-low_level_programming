4850ad36577bf44de829a40c99fab29e3aac071e
Kryuell <Kryuel17@gmail.com>
2023-10-30 18:00:11 +0100

    From Task 0 - 3 (Mandatory)

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * create_file - Creates a file with specific permissions and writes content.
+ * @filename: The name of the file to create.
+ * @text_content: A NULL-terminated string to write to the file.
+ *
+ * Description: This function creates a file with the given filename and
+ * writes the content provided in text_content to it. It sets the file
+ * permissions to 0600 (rw-------).
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, write_len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+			len++;
+
+		write_len = write(fd, text_content, len);
+		if (write_len != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - Copy the contents of one file to another
+ * @ac: The number of arguments
+ * @av: An array of arguments
+ *
+ * Return: 0 on success, other values on failure
+ */
+int main(int ac, char **av)
+{
+	int f, t, n;
+	char b[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f = open(av[1], O_RDONLY);
+	if (f == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		exit(98);
+	}
+	t = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (t == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while ((n = read(f, b, 1024)) > 0)
+	{
+		if (write(t, b, n) != n)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+	if (n == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", av[1]);
+		exit(98);
+	}
+	if (close(f) == -1 || close(t) == -1)
+	{
+		dprintf(2, "Error: Can't close file descriptor\n");
+		exit(100);
+	}
+	return (0);
+}
