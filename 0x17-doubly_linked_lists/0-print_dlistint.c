263cf1de071832877224c63784445a03553511a2
Kryuell <Kryuel17@gmail.com>
2023-12-07 16:26:15 +0100

    Mandatory

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - Print elements of a doubly linked list.
+ * @h: Head of the linked list.
+ *
+ * Return: The size of the linked list.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t size = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        size++;
+        h = h->next;
+    }
+
+    return size;
+}
