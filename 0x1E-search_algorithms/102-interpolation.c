98836bc380b9e1ba32371d7bdb3efd3ec5c4dd1b
Kryuell <Kryuel17@gmail.com>
2024-04-14 19:56:35 +0200

    updated 103-104-105-106

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,54 +1,36 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
-* interpolation_search - Searches for a value using the Interpolation search
-* @array: A pointer to the first element of the array to search in
-* @size: The number of elements in the array
-* @value: The value to search for
-*
-* Return: The first index where value is located, or -1 on failure
-*/
+ * interpolation_search - performs jump search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
+ */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low = 0;
-	size_t high = size - 1;
-	size_t pos;
+	size_t pos = 0, low = 0, high = size - 1;
 
-	if (array == NULL)
+	if (!array || !size)
 		return (-1);
 
-	while (low <= high && value >= array[low] && value <= array[high])
+	while (1)
 	{
-		/* Avoid division by zero */
-		if (low == high)
-		{
-			if (array[low] == value)
-				return (low);
-			break;
-		}
-
-		/* Calculate the probe position */
 		pos = low + (((double)(high - low) /
-			(array[high] - array[low])) * (value - array[low]));
-
-		/* Check if the position is within the array bounds */
+			  (array[high] - array[low])) * (value - array[low]));
 		if (pos >= size)
 		{
 			printf("Value checked array[%lu] is out of range\n", pos);
-			return (-1);
+			break;
 		}
-
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-
 		if (array[pos] == value)
-			return ((int)pos); /* Value found */
-
-		if (array[pos] < value)
-			low = pos + 1; /* Value is in upper part of the range */
+			return (pos);
+		else if (array[pos] > value)
+			high = pos - 1;
 		else
-			high = pos - 1; /* Value is in lower part of the range */
+			low = pos + 1;
 	}
-
-	return (-1); /* Value not found */
+	return (-1);
 }
